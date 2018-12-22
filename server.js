031a46fe6ad3d5f45b2db4cbf170030406e775b8
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-12-23 00:07:00 +0100

    Work commit

--- server.js
+++ server.js
@@ -7,22 +7,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.get('/getNote', function () {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
+    fs.readFile('./test.json', 'utf8', function(req, err, data, res) {
 	    if (err) throw err;
 	    stringifyFile = data
-	    res.send([data]);
+	    res.send(data);
 	});
 });
 
-
-
 app.post('/updateNote/:note', function () {
+	stringifyFile.req.params.note;
     fs.writeFile('./test.json', stringifyFile, function(err) {
 	    if (err) throw err;
 	    console.log('file updated');
 	});
 });
-
-
 
 app.listen(3000);
